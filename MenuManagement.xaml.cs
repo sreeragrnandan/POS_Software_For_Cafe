9217abbf80ebfda700ac6ea7c63a874e036468a0
Maneesh Manoj <41136375+maneeshmanu2000@users.noreply.github.com>
2020-07-05 20:34:24 +0530

    Added a New Menu WIndow

---
+++ MenuManagement.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Callista_Cafe
+{
+    /// <summary>
+    /// Interaction logic for MenuManagement.xaml
+    /// </summary>
+    public partial class MenuManagement : Window
+    {
+        public MenuManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
