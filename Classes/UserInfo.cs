2e3de016bc0885e415f4e1ca81b9984cbdfb308f
Rashi Raffi <Rashi1281@gmail.com>
2020-07-06 13:17:43 +0530

    New Class created for storing Current user information

---
+++ Classes/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callista_Cafe.Classes
+{
+    class UserInfo
+    {
+        public static bool Login;
+        public static int User_Id;
+        public static string User_Name = "";
+        public static int User_Type;
+    }
+}
