de3f0f15cb16314b7df1b2b1e2347eae4172cf07
Maneesh Manoj <41136375+maneeshmanu2000@users.noreply.github.com>
2020-08-02 14:00:03 +0530

    Added Update and Delete Functionality

--- Classes/Supplier.cs
+++ Classes/Supplier.cs
@@ -70,15 +70,52 @@ namespace Callista_Cafe.Classes
             return result;
         }
 
-        public bool update_query()
+        public bool update_query(Supplier sup)
         {
             bool result=false;
+            try
+            {
+                con=new SqlConnection(ConString);
+                cmd = new SqlCommand("Update Suppliers set supplier_name=@sup_name,supplier_mobile=@sup_mobile WHERE supplier_id=@sup_id",con);
+                cmd.Parameters.AddWithValue("@sup_id", sup.supplier_id);
+                cmd.Parameters.AddWithValue("@sup_name", sup.supplier_name);
+                cmd.Parameters.AddWithValue("@sup_mobile", sup.supplier_mobile);
+
+                con.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally{con.Close();}
             return result;
         }
 
-        public bool delete_query()
+        public bool delete_query(Supplier sup)
         {
             bool result=false;
+            con = new SqlConnection(ConString);
+            try
+            {
+                cmd = new SqlCommand("Delete from dbo.suppliers where supplier_id=@sup_id",con);
+                cmd.Parameters.AddWithValue("@sup_id", sup.supplier_id);
+                con.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows>0)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }finally{con.Close();}
             return result;
         }
     }
--- SupplierWindow.xaml.cs
+++ SupplierWindow.xaml.cs
@@ -52,6 +52,7 @@ namespace Callista_Cafe
                         dt = supplier_object.select_query();
                         Suppliers.SetBinding(ItemsControl.ItemsSourceProperty, new Binding { Source = dt });
                         MessageBox.Show("New Supplier Added", "Success");
+                        reset_txtbox();
                     }
                     else
                     {
@@ -68,7 +69,79 @@ namespace Callista_Cafe
                 MessageBox.Show(ex.ToString());
             }
         }
+        private void DeleteSupplierBtnClick(object sender, RoutedEventArgs e)
+        {
+            Supplier dltsupplier_object = new Supplier();
+            MessageBoxResult msgbox = MessageBox.Show("Are You Sure?", "DELETE", MessageBoxButton.YesNo);
+            switch (msgbox)
+            {
+                case MessageBoxResult.Yes:
+                    dltsupplier_object.supplier_id = int.Parse(Txtsupplierid.Text);
+                    bool result = supplier_object.delete_query(dltsupplier_object);
+                    if (result)
+                    {
+                        MessageBox.Show("Succesfully Deleted", "Deleted");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to Delete Item", "Failed");
+                    }
+                    DataTable dt = supplier_object.select_query();
+                    Suppliers.SetBinding(ItemsControl.ItemsSourceProperty, new Binding {Source = dt});
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Not DOne");
+                    break;
+            }
+            reset_txtbox();
+        }
 
+        private void UpdateSupplierBtnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult msgbox = MessageBox.Show("Are You Sure?", "update", MessageBoxButton.YesNo);
+            switch (msgbox)
+            {
+                case MessageBoxResult.Yes:
+                    try
+                    {
+                        if (!Txtsuppliername.Text.ToString().Equals(""))
+                        {
+                            supplier_object.supplier_id = int.Parse(Txtsupplierid.Text.ToString());
+                            supplier_object.supplier_name = Txtsuppliername.Text.ToString();
+                            supplier_object.supplier_mobile = Txtsuppliermobile.Text.ToString();
+
+                            bool update_result = supplier_object.update_query(supplier_object);
+                            if (update_result)
+                            {
+                                DataTable dt = new DataTable();
+                                dt = supplier_object.select_query();
+                                Suppliers.SetBinding(ItemsControl.ItemsSourceProperty, new Binding { Source = dt });
+                                MessageBox.Show("Supplier Updated", "Success");
+                                reset_txtbox();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error Updating Supplier", "Error");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please Enter a Supplier Name to Proceed", "Invalid Entry");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+        private void ResetSupplierBtnClick(object sender, RoutedEventArgs e)
+        {
+            reset_txtbox();
+        }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -83,12 +156,25 @@ namespace Callista_Cafe
             {
                 MessageBox.Show(ex.ToString());
             }
-            finally
+        }
+
+        private void Suppliers_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = (DataGrid)sender;
+            DataRowView selected_row = dg.SelectedItem as DataRowView;
+
+            if (selected_row != null)
             {
-                
+                Txtsupplierid.Text = selected_row[0].ToString();
+                Txtsuppliername.Text = selected_row[1].ToString();
+                Txtsuppliermobile.Text = selected_row[2].ToString();
             }
         }
 
+        public void reset_txtbox()
+        {
+            Txtsuppliername.Text = ""; Txtsuppliermobile.Text = ""; Txtsupplierid.Text = "";
+        }
     }
 }
 
