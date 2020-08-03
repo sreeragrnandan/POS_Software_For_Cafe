0148097eb0d63f1291809ad7c8f11141414974e8
Rashi Raffi <Rashi1281@gmail.com>
2020-08-04 01:40:59 +0530

    Updated MenuItm.cs File
    * Fixed bug on the sql query.
    * Added a fucntion to update selected row on the database.
    * Added con close statement on insert functon.

--- Classes/MenuItm.cs
+++ Classes/MenuItm.cs
@@ -25,7 +25,7 @@ namespace Callista_Cafe.Classes
             try
             {
                 con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
-                cmd = new SqlCommand("SELECT item_id,item_name,item_price,item_category FROM menu_items;", con);
+                cmd = new SqlCommand("SELECT item_id,item_name,item_category,item_price FROM menu_items;", con);
                 SqlDataAdapter itemDataAdapter = new SqlDataAdapter(cmd);
                 con.Open();
                 itemDataAdapter.Fill(itemDataTable);
@@ -71,9 +71,48 @@ namespace Callista_Cafe.Classes
             {
                 MessageBox.Show(e.ToString(), "Error");
             }
+            finally
+            {
+                con.Close();
+            }
             return result;
         }
 
+        public bool update( MenuItm itm)
+        {
+            bool result = false;
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
+            try
+            {
+                cmd = new SqlCommand(
+                    "UPDATE menu_items SET item_name=@name, item_price=@price, item_category=@category WHERE item_id=@id",
+                    con);
+                cmd.Parameters.AddWithValue("@id", itm.item_id);
+                cmd.Parameters.AddWithValue("@name", itm.item_name);
+                cmd.Parameters.AddWithValue("@price", itm.item_price);
+                cmd.Parameters.AddWithValue("@category", itm.item_category);
+                con.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+            }
+            finally
+            {
+                con.Close();
+            }
+            return result;
+        }
 
     }
 }
