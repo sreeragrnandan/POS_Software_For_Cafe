5562795fb3fe38bf39820dda3f6fa3716c0f7f36
Rashi Raffi <Rashi1281@gmail.com>
2020-08-04 01:23:56 +0530

    Updated MenuDashboard.xaml.cs File
    * Added LoadGrid function to Reload the Grid.
    * Added BindDataToObject function to check and bind the data from the TextBox.
    * Added addBtn_Click fucntion to insert new records.
    * Added a reset function to clear all the text box and change button state.
    * Added a menuItems_SelectionChanged funtion to load data from datagrid to the textBox.
    * Added Reset Button Click Fucntion.
    * Added UpdateBtn_Click funtion to update records on database.

--- MenuDashboard.xaml.cs
+++ MenuDashboard.xaml.cs
@@ -26,10 +26,165 @@ namespace Callista_Cafe
             InitializeComponent();
         }
         MenuItm MenuItem = new MenuItm();
+        MenuItm binddataMenuItm = new MenuItm();
         private void MenuItems_Loaded(object sender, RoutedEventArgs e)
         {
+            loadGrid();
+        }
+
+        public void loadGrid()
+        {
             DataTable menuDataTable = MenuItem.select();
-            MenuItems.SetBinding(ItemsControl.ItemsSourceProperty, new Binding {Source = menuDataTable});
+            MenuItems.SetBinding(ItemsControl.ItemsSourceProperty, new Binding { Source = menuDataTable });
+        }
+        private bool BindDataToObject( bool id)
+        {
+            bool flag = true;
+            if (id)
+            {
+                binddataMenuItm.item_id = int.Parse(ItemIDTxtBox.Text.ToString());
+            }
+            else
+            {
+                binddataMenuItm.item_id = 0;
+            }
+
+            if (ItemNameTxtBox.Text.ToString().Equals(""))
+            {
+                MessageBox.Show("Please Enter the Name.", "Info");
+                flag = false;
+                goto FUNEND;
+            }
+            else
+            {
+                try
+                {
+                    binddataMenuItm.item_name = ItemNameTxtBox.Text.ToString();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Please Enter a valid Name.!", "Error");
+                }
+            }
+            if (ItemPriceTxtBox.Text.ToString().Equals(""))
+            {
+                MessageBox.Show("Please Enter the Price.", "Info");
+                flag = false;
+                goto FUNEND;
+            }
+            else
+            {
+                try
+                {
+                    binddataMenuItm.item_price = float.Parse(ItemPriceTxtBox.Text.ToString());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Please Enter a valid price.!", "Error");
+                }
+            }
+            if (ItemCategoryComboBox.Text.ToString().Equals(""))
+            {
+                MessageBox.Show("Please Enter/Select a Category.", "Info");
+                flag = false;
+                goto FUNEND;
+            }
+            else
+            {
+                try
+                {
+                    binddataMenuItm.item_category = ItemCategoryComboBox.Text;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Please Enter a valid price.!", "Error");
+                }
+            }
+            FUNEND:{}
+            return flag;
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool bindingResult = BindDataToObject(false);
+            bool result;
+            if (bindingResult)
+            {
+                result=MenuItem.insert(binddataMenuItm);
+                if (result)
+                {
+                    MessageBox.Show("New Item Inserted..!", "Success");
+                    reset();
+                    loadGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to insert. Try Again !", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong. Please try Again !");
+            }
+        }
+
+        private void reset()
+        {
+            ItemIDTxtBox.Text = "";
+            ItemNameTxtBox.Text = "";
+            ItemCategoryComboBox.Text = "";
+            ItemPriceTxtBox.Text = "";
+            updateBtn.IsEnabled = false;
+            deleteBtn.IsEnabled = false;
+            addBtn.IsEnabled = true;
+            addRequirementBtn.IsEnabled = false;
+        }
+
+        private void MenuItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid gd = (DataGrid)sender;
+            DataRowView row_selected = gd.SelectedItem as DataRowView;
+            if (row_selected != null)
+            {
+                ItemIDTxtBox.Text = row_selected[0].ToString();
+                ItemNameTxtBox.Text = row_selected[1].ToString();
+                ItemCategoryComboBox.Text = row_selected[2].ToString();
+                ItemPriceTxtBox.Text = row_selected[3].ToString();
+                
+                addBtn.IsEnabled = false;
+                updateBtn.IsEnabled = true;
+                deleteBtn.IsEnabled = true;
+                addRequirementBtn.IsEnabled = true;
+            }
+        }
+
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            reset();
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool bindingResult = BindDataToObject(false);
+            bool result;
+            if (bindingResult)
+            {
+                result = MenuItem.insert(binddataMenuItm);
+                if (result)
+                {
+                    MessageBox.Show("Item Successfully Updated !", "Success");
+                    reset();
+                    loadGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to Update. Try Again !", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong. Please try Again !");
+            }
         }
     }
 }
