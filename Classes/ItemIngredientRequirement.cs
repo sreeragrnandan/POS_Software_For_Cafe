1e228621c46e1da51cf3d64312cda8e8c8a4bd85
Rashi Raffi <Rashi1281@gmail.com>
2020-08-04 23:51:44 +0530

    Added New Class FIle ItemIngredientrequirement
    * Added Form variable
    * Added a function to query data from database

---
+++ Classes/ItemIngredientRequirement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Callista_Cafe.Classes
+{
+    class ItemIngredientRequirement
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+
+
+        public int menuItemId { get; set; }
+        public int ingredientId { get; set; }
+        public float ingredientQuantity { get; set; }
+
+        public DataTable select(int menuItemId)
+        {
+            DataTable ReqDataTable = new DataTable();
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
+                cmd = new SqlCommand("SELECT inv.ingredient, ing.quantity, inv.unit FROM inventory AS inv, ingredient_list AS ing WHERE ing.menu_item_id=@menuItemId and inv.id=ing.ingredient_id;", con);
+                cmd.Parameters.AddWithValue("@menuItemId", menuItemId);
+                SqlDataAdapter Adapter = new SqlDataAdapter(cmd);
+                con.Open();
+                Adapter.Fill(ReqDataTable);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return ReqDataTable;
+        }
+    }
+}
