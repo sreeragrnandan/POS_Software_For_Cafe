e68d5c2e2959e3a23d64abe4576459ea18f3c57c
Rashi Raffi <Rashi1281@gmail.com>
2020-08-04 17:52:20 +0530

    Added MenuItemRequirement Window

---
+++ MenuItemRequirement.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Callista_Cafe.Classes;
+
+namespace Callista_Cafe
+{
+    /// <summary>
+    /// Interaction logic for MenuItemRequirement.xaml
+    /// </summary>
+    public partial class MenuItemRequirement : Window
+    {
+        private int menuItemId;
+        public MenuItemRequirement()
+        {
+            InitializeComponent();
+        }
+        public MenuItemRequirement(int Menu_Item_ID) : this()
+        {
+            this.menuItemId = Menu_Item_ID;
+        }
+    }
+}
